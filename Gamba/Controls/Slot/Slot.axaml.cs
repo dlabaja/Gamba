341a084aae9152f6824f181f5fc84553e2a3065e
dlabaja <dlabaja.jan@seznam.cz>
2025-12-04 22:25:07 +0100

    emoji v ui

--- Gamba/Controls/Slot/Slot.axaml.cs
+++ Gamba/Controls/Slot/Slot.axaml.cs
@@ -1,12 +1,45 @@
+using Avalonia;
 using Avalonia.Controls;
+using Gamba.Enums;
 
 namespace Gamba.Controls.Slot;
 
 public partial class Slot : UserControl
 {
+    public static readonly StyledProperty<SlotSymbol> SymbolProperty =
+        AvaloniaProperty.Register<Slot, SlotSymbol>(nameof(Symbol));
+
+    private string GetStringSymbol(SlotSymbol symbol)
+    {
+        switch (symbol)
+        {
+            case SlotSymbol.BAR:
+                return "🍫";
+            case SlotSymbol.CHERRY:
+                return "🍒";
+            case SlotSymbol.LEMON:
+                return "🍋";
+            case SlotSymbol.DIAMOND:
+                return "💎";
+            case SlotSymbol.ORANGE:
+                return "🍊";
+            default:
+                return "";
+        }
+    }
+    
+    public string SymbolString => GetStringSymbol(Symbol);
+    
+    public SlotSymbol Symbol
+    {
+        get => GetValue(SymbolProperty);
+        set => SetValue(SymbolProperty, value);
+    }
+    
     public Slot()
     {
         InitializeComponent();
+        DataContext = this;
     }
 }
 
