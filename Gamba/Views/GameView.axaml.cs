bab903597e3945aef1b875a8d6b78265d615ac3e
dlabaja <dlabaja.jan@seznam.cz>
2025-12-07 13:48:55 +0100

    slot machine ui

--- Gamba/Views/GameView.axaml.cs
+++ Gamba/Views/GameView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
 using Avalonia.Threading;
 using Gamba.ViewModels;
 using System;
@@ -10,6 +11,9 @@ namespace Gamba.Views;
 public partial class GameView : UserControl
 {
     private int currentCount = 0;
+    private bool isHandleDown = false;
+    private readonly Bitmap handleDown = new Bitmap("./Assets/img/lever-down.png");
+    private readonly Bitmap handleUp = new Bitmap("./Assets/img/lever-up.png");
 
     public GameView()
     {
@@ -28,5 +32,12 @@ public partial class GameView : UserControl
                 CanvasPanel.Margin = new Thickness(0, 0, 0, -currentCount * 100);
             },
             DispatcherPriority.Background);
+    }
+
+    private void Handle_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Handle.Source = this.isHandleDown ? this.handleUp : this.handleDown;
+
+        this.isHandleDown = !this.isHandleDown;
     }
 }
