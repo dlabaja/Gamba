cf435f004ddccf1aaf24c88a97ca3ff4dc4b12f4
dlabaja <dlabaja.jan@seznam.cz>
2025-12-07 12:27:57 +0100

    roll end delay

--- Gamba/ViewModels/Commands/RollCommand.cs
+++ Gamba/ViewModels/Commands/RollCommand.cs
@@ -1,4 +1,4 @@
-using Gamba.Models;
+using Avalonia.Threading;
 using System;
 using System.Windows.Input;
 
@@ -8,8 +8,19 @@ public class RollCommand : ICommand
 {
     public event EventHandler? CanExecuteChanged;
     public event EventHandler? OnExecute;
+    private int counter = 3;
 
-    public bool CanExecute(object? parameter) => true;
+    public bool CanExecute(object? parameter) => this.counter > 2;
+
+    public RollCommand()
+    {
+        Controller.Game.OnNextRoll += (_, _) =>
+        {
+            counter++;
+            Console.WriteLine(counter);
+            RaiseCanExecuteChanged();
+        };
+    }
 
     public void Execute(object? parameter)
     {
@@ -20,7 +31,18 @@ public class RollCommand : ICommand
         else
         {
             Controller.Game.StartRoll();
+            counter = 0;
+            RaiseCanExecuteChanged();
         }
         this.OnExecute?.Invoke(this, EventArgs.Empty);
+    }
+    
+    private void RaiseCanExecuteChanged()
+    {
+        // bez tohodle mi to zmrazilo UI
+        Dispatcher.UIThread.Post(() =>
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        });
     }
 }
